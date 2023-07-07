5cb6226b1cc773e75d1f9e0e97e7f2e2c7bfd97c
wangyunhao <1595014015@qq.com>
2023-07-07 16:47:07 +0800

    07-springboot-devtools 用devtools热部署应用，需要浏览器安装liveload插件

---
+++ 07-springboot-devtools/src/main/java/com/pdai/springboothelloworld/App.java
@@ -0,0 +1,22 @@
+package com.pdai.springboothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@GetMapping("/hello")
+	public ResponseEntity<String> hello() {
+		return new ResponseEntity<>("helloworld!!!", HttpStatus.OK);
+	}
+}

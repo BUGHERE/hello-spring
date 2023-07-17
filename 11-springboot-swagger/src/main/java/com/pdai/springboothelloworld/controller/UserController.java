56c9b30a265121bbb0139d229dbe0756c74ac3e3
wangyunhao <1595014015@qq.com>
2023-07-18 03:00:07 +0800

    11-springboot-swagger 使用swagger生成Api文档

---
+++ 11-springboot-swagger/src/main/java/com/pdai/springboothelloworld/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.pdai.springboothelloworld.controller;
+
+import com.pdai.springboothelloworld.entity.User;
+import com.pdai.springboothelloworld.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Api
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @ApiOperation("Add User")
+    @ApiImplicitParam(name = "user", dataTypeClass = User.class, required = true)
+    @RequestMapping("/add")
+    public User add(User user) {
+        userService.addUser(user);
+        return user;
+    }
+
+    @ApiOperation("Query User List")
+    @RequestMapping("/list")
+    public List<User> list() {
+        return userService.list();
+    }
+}

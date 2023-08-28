ff1324b8ace36f0b2b85804fb49b9d478c03798e
wangyunhao <1595014015@qq.com>
2023-08-28 20:46:12 +0800

    springboot-mybatis-plus: fix

--- everywhere/springboot-mybatis-plus/src/main/java/com/example/demo/mapper/EmployeeMapper.java
+++ everywhere/springboot-mybatis-plus/src/main/java/com/example/demo/mapper/EmployeeMapper.java
@@ -3,10 +3,12 @@ package com.example.demo.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.demo.entity.Employee;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
+@Mapper
 public interface EmployeeMapper extends BaseMapper<Employee> {
     // 实现在 EmployeeMapper.xml 中
     // 这里的xml文件路径不需要在application.yml中配置，因为mybatis-plus默认配置在resources/mapper下
